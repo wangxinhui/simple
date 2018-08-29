d3089f5f8bc16437845ceac37d3e38b2031e3eb3
wangxinhui <wangxinhuileft@163.com>
2018-08-29 20:49:50 +0800

    xml is learning now

--- src/test/java/chapter10/com/simple/jdbc/JdbcConfig.java
+++ src/test/java/chapter10/com/simple/jdbc/JdbcConfig.java
@@ -1,29 +1,30 @@
 package chapter10.com.simple.jdbc;
 
-import javax.sql.DataSource;
-
 import com.simple.spring4.chapter10.db.SpitterRepository;
 import com.simple.spring4.chapter10.db.SpittleRepository;
 import com.simple.spring4.chapter10.db.jdbc.JdbcSpitterRepository;
 import com.simple.spring4.chapter10.db.jdbc.JdbcSpittleRepository;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 
 @Configuration
 public class JdbcConfig {
 
-  @Bean
+  @Bean(name = "ds")
   public DataSource dataSource() {
-    return new EmbeddedDatabaseBuilder()
-      .setType(EmbeddedDatabaseType.H2)
-      .addScripts("classpath:chapter10/spittr/db/jdbc/schema.sql", "classpath:chapter10/spittr/db/jdbc/test-data.sql")
-      .build();
+    BasicDataSource ds = new BasicDataSource();
+    ds.setDriverClassName("com.mysql.jdbc.Driver");
+    ds.setUrl("jdbc:mysql://localhost:3306/crrw?useUnicode=true&characterEncoding=UTF-8");
+    ds.setUsername("root");
+    ds.setPassword("123456");
+    return ds;
   }
   
   @Bean
